a181c537d168ed95ae2dc9db6cdc5337ac468b45
Kevin Kago <kavin.kago@student.moringaschool.com>
2025-03-12 10:00:00 +0300

    initial commit

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,24 @@
+
 #!/usr/bin/env python3
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_sandbox import Student
+
+def test_student_table_exists():
+    assert hasattr(Student, '__tablename__')
+
 
 Base = declarative_base()
 
 class Student(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
     pass
 
 if __name__ == '__main__':
+    engine =create_engine('sqlite:///students.db')
+    Base.metadata.create_all(engine)
     pass
